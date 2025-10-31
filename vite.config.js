87e8f59901be6d71926be993cc2763a7cefde340
thibakars-cmyk <thibakar.s@gmail.com>
2025-10-30 23:43:06 -0400

    t

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.', // ensure root is project root
+  build: {
+    outDir: 'dist',
+  },
 })
